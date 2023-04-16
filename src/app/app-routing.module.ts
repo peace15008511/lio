10406362e0b850809ab7f26044e9c61670b9e13d
Peace Mulovhedzi <Peace.Mulovhedzi@adaptit.com>
2023-04-16 10:18:05 +0200

    initial commit with few components and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: '',
+    pathMatch: 'full',
+  },
+  {
+    path: 'about-me',
+    component: AboutMeComponent,
+    title: 'Contact US',
+    pathMatch: 'full',
+  },
+  { path: '**', component: NotFoundComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectCardComponent,
+    NotFoundComponent,
+    AboutMeComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
